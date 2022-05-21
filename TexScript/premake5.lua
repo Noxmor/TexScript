e489df04ba9f5df84c752d8a7b73a311a386b95a
Noxmor <janek.drews@t-online.de>
2022-05-21 16:13:50 +0200

    Added Hello World
    Added Hello World Program for testing and general project architecture

---
+++ TexScript/premake5.lua
@@ -0,0 +1,47 @@
+project "TexScript"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "tspch.hpp"
+	pchsource "src/tspch.cpp"
+
+	files
+	{
+		"src/**.h",
+		"src/**.hpp",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"src"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		filter "configurations:Debug"
+		defines "TS_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+
+	filter "configurations:Release"
+		defines "TS_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "´TS_DIST"
+		runtime "Release"
+		optimize "on"
---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "TexScript"
+	architecture "x86_64"
+	startproject "TexScript"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "TexScript"
